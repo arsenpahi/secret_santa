8c345b8c6f70e1132bb7bd795a74275219177777
Arsen Pahi <arsen.pahi@gmail.com>
2018-01-14 22:51:33 +0300

    test deploy

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -21,7 +21,7 @@
 						<span class="input__label-content">Password *</span>
 					</label>
 				</span>
-        <button v-on:click="signIn" >Sign In</button>
+        <button v-on:click="signIn" >Sigfn In</button>
     </div>
 </template>
 <script>
